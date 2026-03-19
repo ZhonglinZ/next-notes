38937f52a725584169c7f7762498a7b04701c73b
Zhonglin.Zhao <1525628693@qq.com>
2026-03-19 08:51:01 +0800

    feat: 笔记预览

---
+++ app/note/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Note from "@/components/Note";
+import { getNote } from "@/lib/redis";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  // 动态路由 获取笔记 id
+  const { id: noteId } = await params;
+  const note = await getNote(noteId);
+
+  // 为了让 Suspense 的效果更明显
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+  await sleep(2000);
+
+  if (note == null) {
+    return (
+      <div className="note--empty-state">
+        <span className="note-text--empty-state">
+          Click a note on the left to view something! 🥺
+        </span>
+      </div>
+    );
+  }
+
+  return <Note noteId={noteId} note={note} />;
+}
